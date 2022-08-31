2bba1415226443780f59bcb770aaade1ab9e00fa
kakyoindonut321 <forbiddenikki@gmail.com>
2022-09-01 06:27:37 +0700

    commit kedua

---
+++ script.js
@@ -0,0 +1,31 @@
+function getComputerChoice() {
+    let rng = Math.floor(Math.random() * 3);
+    if (rng == 0) {
+        return let = "batu";
+    } else if (rng == 1) {
+        return let = "gunting";
+    } else if (rng == 2) {
+        return let = "kertas";
+    }
+
+}
+
+function togglePage(page) {
+    var pageOne = document.getElementByClass("Pertama");
+    var pageTwo = document.getElementById("Kedua");
+    var pageThree = document.getElementById("Ketiga");
+    var pageFour = document.getElementById("Keempat");
+    let viv = "berhasil";
+    switch (page) {
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+    }
+
+    return viv;
+}
